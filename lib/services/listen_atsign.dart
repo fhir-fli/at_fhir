b0f7bf0a259b5cdb738e400644adf84ba0768b3c
Dokotela <grey.faulkenberry@mayjuun.com>
2023-05-28 12:34:26 -0400

    Listener doesn't work right

--- lib/services/listen_atsign.dart
+++ lib/services/listen_atsign.dart
@@ -1,10 +1,11 @@
-import 'package:at_client/at_client.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'dart:async';
 
+import 'package:at_client/at_client.dart';
 import 'package:fhir/dstu2.dart' as dstu2;
 import 'package:fhir/r4.dart' as r4;
 import 'package:fhir/r5.dart' as r5;
 import 'package:fhir/stu3.dart' as stu3;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../at_fhir.dart';
 
@@ -12,39 +13,91 @@ part 'listen_atsign.g.dart';
 
 AtClient atClient = AtClientManager.getInstance().atClient;
 
+/// Provider to listen to any changes of any FHIR resources
 @riverpod
-Stream<dynamic> values(ValuesRef ref) {
-  atClient.notificationService
-      .subscribe(
-          regex:
-              'fhir.(dstu2|stu3|r4|r5).($resourceTypesRegex).[A-Za-z0-9\-\.]{1,64}',
-          shouldDecrypt: true)
-      .listen(((notification) async {
-    notification.value;
-    final keyList = notification.key.split('.');
-    if (keyList.length > 3) {
-      final versionString = keyList[1];
-      if (['dstu2', 'stu3', 'r4', 'r5'].contains(versionString)) {
-        switch (versionString) {
-          case 'dstu2':
-            return dstu2.Resource.fromJsonString(notification.value!);
-            break;
-          case 'stu3':
-            stu3.Resource.fromJsonString(notification.value!);
-            break;
-          case 'r4':
-            r4.Resource.fromJsonString(notification.value!);
-            break;
-          case 'r5':
-            r5.Resource.fromJsonString(notification.value!);
-            break;
-          default:
+class AtSignStream extends _$AtSignStream {
+  StreamSubscription<dynamic> build() => atClient.notificationService
+          .subscribe(
+              regex:
+                  'fhir\.(dstu2|stu3|r4|r5)\.($resourceTypesRegex)\.[A-Za-z0-9\-\.]{1,64}',
+              shouldDecrypt: true)
+          .listen((event) async* {
+        final keyList = event.key.split('.');
+
+        /// Check to ensure the key has at least the 4 parts of a FHIR resource
+        /// at an atsign (fhir.version.resourceType.id)
+        if (keyList.length > 3 && event.value != null) {
+          switch (keyList[1]) {
+            case 'dstu2':
+              yield dstu2.Resource.fromJsonString(event.value!);
+            case 'stu3':
+              yield stu3.Resource.fromJsonString(event.value!);
+            case 'r4':
+              yield r4.Resource.fromJsonString(event.value!);
+            case 'r5':
+              yield r5.Resource.fromJsonString(event.value!);
+            default:
+          }
         }
-      } else {
-        // TODO(Dokotela): handle inappropriate version
-      }
-    } else {
-      // TODO(Dokotela): handle wrong list length
-    }
-  }), onError: (e) {}, onDone: () {});
+      });
+}
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+@riverpod
+class AtSignResourceTypeStream extends _$AtSignResourceTypeStream {
+  StreamSubscription<dynamic> build(String resourceType) =>
+      atClient.notificationService
+          .subscribe(
+              // regex:
+              //     'fhir.*', //\.(dstu2|stu3|r4|r5)\.$resourceType\.[A-Za-z0-9\-\.]{1,64}',
+              shouldDecrypt: true)
+          .listen((event) async* {
+        print('Key: ${event.key}');
+        final keyList = event.key.split('.');
+
+        /// Check to ensure the key has at least the 4 parts of a FHIR resource
+        /// at an atsign (fhir.version.resourceType.id)
+        if (keyList.length > 3 && event.value != null) {
+          switch (keyList[1]) {
+            case 'dstu2':
+              yield dstu2.Resource.fromJsonString(event.value!);
+            case 'stu3':
+              yield stu3.Resource.fromJsonString(event.value!);
+            case 'r4':
+              yield r4.Resource.fromJsonString(event.value!);
+            case 'r5':
+              yield r5.Resource.fromJsonString(event.value!);
+            default:
+          }
+        }
+      });
+}
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+@riverpod
+class AtSignResourceStream extends _$AtSignResourceStream {
+  StreamSubscription<dynamic> build(String resourceType, String id) =>
+      atClient.notificationService
+          .subscribe(
+              regex: 'fhir.(dstu2|stu3|r4|r5).$resourceType.$id',
+              shouldDecrypt: true)
+          .listen((event) async* {
+        final keyList = event.key.split('.');
+
+        /// Check to ensure the key has at least the 4 parts of a FHIR resource
+        /// at an atsign (fhir.version.resourceType.id)
+        if (keyList.length > 3 && event.value != null) {
+          switch (keyList[1]) {
+            case 'dstu2':
+              yield dstu2.Resource.fromJsonString(event.value!);
+            case 'stu3':
+              yield stu3.Resource.fromJsonString(event.value!);
+            case 'r4':
+              yield r4.Resource.fromJsonString(event.value!);
+            case 'r5':
+              yield r5.Resource.fromJsonString(event.value!);
+            default:
+          }
+        }
+      });
 }
--- lib/services/listen_atsign.g.dart
+++ lib/services/listen_atsign.g.dart
@@ -6,18 +6,276 @@ part of 'listen_atsign.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$valuesHash() => r'37b1387a82221bd7fb51862b69fd974539c571a4';
+String _$atSignStreamHash() => r'd121bd614228bb8b792af689cecc4308620ff4e7';
 
-/// See also [values].
-@ProviderFor(values)
-final valuesProvider = AutoDisposeStreamProvider<dynamic>.internal(
-  values,
-  name: r'valuesProvider',
+/// Provider to listen to any changes of any FHIR resources
+///
+/// Copied from [AtSignStream].
+@ProviderFor(AtSignStream)
+final atSignStreamProvider = AutoDisposeNotifierProvider<AtSignStream,
+    StreamSubscription<dynamic>>.internal(
+  AtSignStream.new,
+  name: r'atSignStreamProvider',
   debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$valuesHash,
+      const bool.fromEnvironment('dart.vm.product') ? null : _$atSignStreamHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef ValuesRef = AutoDisposeStreamProviderRef<dynamic>;
+typedef _$AtSignStream = AutoDisposeNotifier<StreamSubscription<dynamic>>;
+String _$atSignResourceTypeStreamHash() =>
+    r'73bfaa9a086429a987b521db993abe57baf8630c';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+abstract class _$AtSignResourceTypeStream
+    extends BuildlessAutoDisposeNotifier<StreamSubscription<dynamic>> {
+  late final String resourceType;
+
+  StreamSubscription<dynamic> build(
+    String resourceType,
+  );
+}
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+///
+/// Copied from [AtSignResourceTypeStream].
+@ProviderFor(AtSignResourceTypeStream)
+const atSignResourceTypeStreamProvider = AtSignResourceTypeStreamFamily();
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+///
+/// Copied from [AtSignResourceTypeStream].
+class AtSignResourceTypeStreamFamily
+    extends Family<StreamSubscription<dynamic>> {
+  /// Provider to listen to any changes to FHIR resources of the specified type
+  ///
+  /// Copied from [AtSignResourceTypeStream].
+  const AtSignResourceTypeStreamFamily();
+
+  /// Provider to listen to any changes to FHIR resources of the specified type
+  ///
+  /// Copied from [AtSignResourceTypeStream].
+  AtSignResourceTypeStreamProvider call(
+    String resourceType,
+  ) {
+    return AtSignResourceTypeStreamProvider(
+      resourceType,
+    );
+  }
+
+  @override
+  AtSignResourceTypeStreamProvider getProviderOverride(
+    covariant AtSignResourceTypeStreamProvider provider,
+  ) {
+    return call(
+      provider.resourceType,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'atSignResourceTypeStreamProvider';
+}
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+///
+/// Copied from [AtSignResourceTypeStream].
+class AtSignResourceTypeStreamProvider extends AutoDisposeNotifierProviderImpl<
+    AtSignResourceTypeStream, StreamSubscription<dynamic>> {
+  /// Provider to listen to any changes to FHIR resources of the specified type
+  ///
+  /// Copied from [AtSignResourceTypeStream].
+  AtSignResourceTypeStreamProvider(
+    this.resourceType,
+  ) : super.internal(
+          () => AtSignResourceTypeStream()..resourceType = resourceType,
+          from: atSignResourceTypeStreamProvider,
+          name: r'atSignResourceTypeStreamProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$atSignResourceTypeStreamHash,
+          dependencies: AtSignResourceTypeStreamFamily._dependencies,
+          allTransitiveDependencies:
+              AtSignResourceTypeStreamFamily._allTransitiveDependencies,
+        );
+
+  final String resourceType;
+
+  @override
+  bool operator ==(Object other) {
+    return other is AtSignResourceTypeStreamProvider &&
+        other.resourceType == resourceType;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, resourceType.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+
+  @override
+  StreamSubscription<dynamic> runNotifierBuild(
+    covariant AtSignResourceTypeStream notifier,
+  ) {
+    return notifier.build(
+      resourceType,
+    );
+  }
+}
+
+String _$atSignResourceStreamHash() =>
+    r'fe07cf754d496857729b3bcf0911ad63b99f89f4';
+
+abstract class _$AtSignResourceStream
+    extends BuildlessAutoDisposeNotifier<StreamSubscription<dynamic>> {
+  late final String resourceType;
+  late final String id;
+
+  StreamSubscription<dynamic> build(
+    String resourceType,
+    String id,
+  );
+}
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+///
+/// Copied from [AtSignResourceStream].
+@ProviderFor(AtSignResourceStream)
+const atSignResourceStreamProvider = AtSignResourceStreamFamily();
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+///
+/// Copied from [AtSignResourceStream].
+class AtSignResourceStreamFamily extends Family<StreamSubscription<dynamic>> {
+  /// Provider to listen to any changes to FHIR resources of the specified type
+  ///
+  /// Copied from [AtSignResourceStream].
+  const AtSignResourceStreamFamily();
+
+  /// Provider to listen to any changes to FHIR resources of the specified type
+  ///
+  /// Copied from [AtSignResourceStream].
+  AtSignResourceStreamProvider call(
+    String resourceType,
+    String id,
+  ) {
+    return AtSignResourceStreamProvider(
+      resourceType,
+      id,
+    );
+  }
+
+  @override
+  AtSignResourceStreamProvider getProviderOverride(
+    covariant AtSignResourceStreamProvider provider,
+  ) {
+    return call(
+      provider.resourceType,
+      provider.id,
+    );
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'atSignResourceStreamProvider';
+}
+
+/// Provider to listen to any changes to FHIR resources of the specified type
+///
+/// Copied from [AtSignResourceStream].
+class AtSignResourceStreamProvider extends AutoDisposeNotifierProviderImpl<
+    AtSignResourceStream, StreamSubscription<dynamic>> {
+  /// Provider to listen to any changes to FHIR resources of the specified type
+  ///
+  /// Copied from [AtSignResourceStream].
+  AtSignResourceStreamProvider(
+    this.resourceType,
+    this.id,
+  ) : super.internal(
+          () => AtSignResourceStream()
+            ..resourceType = resourceType
+            ..id = id,
+          from: atSignResourceStreamProvider,
+          name: r'atSignResourceStreamProvider',
+          debugGetCreateSourceHash:
+              const bool.fromEnvironment('dart.vm.product')
+                  ? null
+                  : _$atSignResourceStreamHash,
+          dependencies: AtSignResourceStreamFamily._dependencies,
+          allTransitiveDependencies:
+              AtSignResourceStreamFamily._allTransitiveDependencies,
+        );
+
+  final String resourceType;
+  final String id;
+
+  @override
+  bool operator ==(Object other) {
+    return other is AtSignResourceStreamProvider &&
+        other.resourceType == resourceType &&
+        other.id == id;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, resourceType.hashCode);
+    hash = _SystemHash.combine(hash, id.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+
+  @override
+  StreamSubscription<dynamic> runNotifierBuild(
+    covariant AtSignResourceStream notifier,
+  ) {
+    return notifier.build(
+      resourceType,
+      id,
+    );
+  }
+}
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
